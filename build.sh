6f4b6a7f5f1cf8a6623c9bdeb23ee490cc2d2c7f
masaki.ono <aaikmnoos@gmail.com>
2022-07-14 13:16:17 +0900

    Add image sample

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 DIST_DIR="$(pwd)/public"
 DOCUMENT_DIR="$(pwd)/documents"
 
-if [ -d $DIST_DIR/images ]; then rm -Rf $DIST_DIR/images; fi
-mkdir -p $DIST_DIR/images
+if [ -d $DIST_DIR/_images ]; then rm -Rf $DIST_DIR/_images; fi
+mkdir -p $DIST_DIR/_images
 
 asciidoctor \
-  -a imagesdir@=$DOCUMENT_DIR/images \
+  -a imagesdir@=$DOCUMENT_DIR/_images \
   -a docroot=$DOCUMENT_DIR \
-  -a imagesoutdir=$DIST_DIR/images \
+  -a imagesoutdir=$DIST_DIR/_images \
   -o $DIST_DIR/index.html \
   -r asciidoctor-diagram \
   -r asciidoctor-mathematical \
   -b html \
   $DOCUMENT_DIR/index.adoc
-
+cp -r $DOCUMENT_DIR/_images/* $DIST_DIR/_images/
